2159b90b068369ff40be5b7c6aa169ea696da941
lenaSalamat <lenasalamat96@gmail.com>
2017-08-26 09:13:16 +0300

    stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,15 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
-      // write me
+    this._storage ={};
+    this._count=0
+    this.add = function(value){
+     this._storage[value]=value;
+     this._count++;
     };
     this.remove = function() {
-      // write me
+      var del=this._storage[value];
+         delete this._storage[value];
+         this._count--;
+         return del;
     };
+
   };
